03e0af204279797f42ead2a4dc4490a24ad06ac3
vijay <vijay.y@ajairobotics.com>
2025-04-10 11:57:38 +0530

    basic architecture changes

---
+++ pos/apps/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+   
+path('account_login/', views.account_login, name='account_login' ),
+    
+    
+]
--- testing/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import add_item, get_items, lol
-
-urlpatterns = [
-    path('add-item/', add_item, name='add-item'),
-    path('get-items/', get_items, name='get-items'),
-    path('lol/', lol, name='lol'),
-]
